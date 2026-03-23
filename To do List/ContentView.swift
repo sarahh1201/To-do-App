a95d056dc656a47f300c3be85d35f4d2955f8926
Sarah <shy_h@telus.net>
2026-03-22 21:48:51 -0700

    Initial Commit

---
+++ To do List/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  To do List
+//
+//  Created by Sarah Hill on 2026-03-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ To do List/To_do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_do_ListApp.swift
+//  To do List
+//
+//  Created by Sarah Hill on 2026-03-22.
+//
+
+import SwiftUI
+
+@main
+struct To_do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
